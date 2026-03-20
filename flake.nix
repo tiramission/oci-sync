302246a3992b9658b47a08d8d1a90c459956d56d
tiramission <37465086+tiramission@users.noreply.github.com>
2026-03-20 19:57:23 +0800

    feat(nix): generate shell completions via installShellFiles

--- flake.nix
+++ flake.nix
@@ -23,6 +23,15 @@
           # Avoid running network-dependent tests during packaging
           # doCheck = false;
 
+          nativeBuildInputs = [ pkgs.installShellFiles ];
+
+          postInstall = ''
+            installShellCompletion --cmd oci-sync \
+              --bash <($out/bin/oci-sync completion bash) \
+              --zsh <($out/bin/oci-sync completion zsh) \
+              --fish <($out/bin/oci-sync completion fish)
+          '';
+
           meta = with pkgs.lib; {
             description = "Sync local files to OCI-compatible image registries";
             homepage = "https://github.com/tiramission/oci-sync";
